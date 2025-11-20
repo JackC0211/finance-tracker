ee6887cdfe8794b48df76dceadddb60108730bf6
Jack Cornell <jackcornell0211@icloud.com>
2025-11-20 21:58:17 +0000

    Test1

---
+++ Main.py
@@ -0,0 +1,15 @@
+import json
+from datetime import date
+
+DATA_FILE = "finances.json"
+
+def load_data():
+	with open(DATA_FILE, "r") as file:
+		return json.load(file)
+	
+def save_data(data):
+	with open(DATA_FILE, "w") as file:
+		data_json = json.dumps(data)
+		file.write(data_json)
+
+print(load_data()[0])
